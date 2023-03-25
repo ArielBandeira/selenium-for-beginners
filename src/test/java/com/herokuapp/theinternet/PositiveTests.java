c59c8482b9f64fff7b2ad8530e2454e8ecf01111
ariel <arieltwain@gmail.com>
2023-03-24 23:20:25 -0300

    added enter username, enter password and login button

--- src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -1,6 +1,8 @@
 package com.herokuapp.theinternet;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
@@ -29,13 +31,39 @@ public class PositiveTests {
         sleep(2000);
 
     //enter username
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
+
+    //sleep for 1 seconds
+        sleep(1000);
+
     //enter password
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("SuperSecretPassword!");
+
+    //sleep for 3 seconds
+        sleep(3000);
+
+
     //click login button
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+        loginButton.click();
+
+    //sleep for 5 seconds
+        sleep(5000);
 
     //verifications:
     //new url
+    //    String url = "http://the-internet.herokuapp.com/secure";
+    //    driver.get(url);
+    //    System.out.println("chegou na página depois de login");
+
     //logout button is visible
+        WebElement logoutButton = driver.findElement(By.xpath("//a[@class='button secondary radius']"));
+
     //successful login message
+        WebElement successMessage = driver.findElement(By.cssSelector("#flash"));
+
     //close browser
         driver.quit();
 
