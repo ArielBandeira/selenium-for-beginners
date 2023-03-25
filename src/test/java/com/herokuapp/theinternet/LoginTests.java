f61bebe885f13b914bd15372939a0b71dc2dac61
ariel <arieltwain@gmail.com>
2023-03-25 16:07:48 -0300

    adding all login tests to one class

--- src/test/java/com/herokuapp/theinternet/LoginTests.java
+++ src/test/java/com/herokuapp/theinternet/LoginTests.java
@@ -5,11 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class PositiveTests {
-    @Test
-    public void loginTest() {
+public class LoginTests {
+    @Test(priority = 1, groups = { "positiveTests", "smokeTests" })
+    public void positiveLoginTest() {
         System.out.println("Starting login test");
 
 
@@ -64,6 +65,55 @@ public class PositiveTests {
         driver.quit();
 
    }
+
+    @Parameters({ "username", "password", "expectedMessage" })
+    @Test(priority = 2, groups = { "negativeTests", "smokeTests" })
+    public void negativeLoginTest(String username, String password, String expectedFailMessage) {
+
+        System.out.println("Starting negativeLoginTest with " + username + " and " + password);
+
+        //CREATE DRIVER
+        System.setProperty("webdriver.com.edge.driver", "src/main/resources/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+
+        //sleep for 3 seconds
+        sleep(3000);
+
+        //maximize browser window
+        driver.manage().window().maximize();
+
+        //open test page
+        String url = "https://the-internet.herokuapp.com/login";
+        driver.get(url);
+
+        //verifications:
+        //incorrect username and correct password
+
+        //enter incorrect username
+        WebElement usernameElement = driver.findElement(By.xpath("/html//input[@id='username']"));
+        usernameElement.sendKeys(username);
+
+        //enter correct password
+        WebElement passwordElement = driver.findElement(By.name("password"));
+        passwordElement.sendKeys(password);
+
+        //click login button
+        WebElement loginButton = driver.findElement(By.xpath("//form[@id='login']/button[@class='radius']"));
+        loginButton.click();
+
+        //sleep for 3 seconds
+        sleep(5000);
+
+        //fail login message
+        WebElement failMessage = driver.findElement(By.xpath("/html//div[@id='flash']"));
+        String actualFailMessage = failMessage.getText();
+        Assert.assertTrue(actualFailMessage.contains(expectedFailMessage), "Fail message is not the same as expected\nActual message: "
+                + actualFailMessage + "\nExpected message: " + expectedFailMessage);
+
+        //close browser
+        driver.quit();
+
+    }
 
     private static void sleep(long m) {
         try {
