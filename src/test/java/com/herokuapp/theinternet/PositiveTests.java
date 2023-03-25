0084d15708ebac749d69972b91a487a1d83ce180
ariel <arieltwain@gmail.com>
2023-03-24 21:08:55 -0300

    creating first test class

---
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -0,0 +1,28 @@
+package com.herokuapp.theinternet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class PositiveTests {
+
+    public void loginTest() {
+
+        // CREATE DRIVER
+        System.setProperty("webdriver.com.edge.driver", "src/main/resources/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+
+//         open test page
+//         enter username
+//         enter password
+//         click login button
+
+//         verifications:
+//         new url
+//         logout button is visible
+//         successful login message
+
+
+
+   }
+
+}
